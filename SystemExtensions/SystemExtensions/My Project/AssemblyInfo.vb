64fb68f691a8e270bf98afc0efb1181df98f75da
T. Korn <email@tobiaskorn.de>
2020-03-23 19:40:42 +0100

    fixed version format

--- SystemExtensions/SystemExtensions/My Project/AssemblyInfo.vb
+++ SystemExtensions/SystemExtensions/My Project/AssemblyInfo.vb
@@ -13,27 +13,5 @@ Imports System.Runtime.InteropServices
 <Assembly: ComVisible(False)>
 <Assembly: Guid("e8c74000-040a-480d-8519-6a39e15fb6a0")>
 
-<Assembly: AssemblyVersion(Major + "." + Minor + "." + Fix + "." + BuildNumber)>
-<Assembly: AssemblyInformationalVersion(Major + "." + Minor + "." + Fix + "-" + BuildType)>
-
-Public Module SemanticVersion
-
-  'increment this on breaking change:
-  Public Const Major = "2"
-
-  'increment this on new feature (w/o breaking change):
-  Public Const Minor = "3"
-
-  'increment this on internal fix (w/o breaking change):
-  Public Const Fix = "1"
-
-  'AND DONT FORGET TO UPDATE THE VERSION-INFO OF THE *.nuspec FILE!!!
-#Region "..."
-
-  'dont touch this, beacuse it will be replaced ONLY by the build process!!!
-
-  Public Const BuildNumber = "*"
-  Public Const BuildType = "LOCALBUILD"
-
-#End Region
-End Module
+<Assembly: AssemblyVersion("2.3.1.*")>
+<Assembly: AssemblyInformationalVersion("2.3.1-localbuild")>
